1809743d592cb19803bd6e2067372fa757c6575a
rubikmm <rubikmm@gmail.com>
2020-11-18 00:08:58 +0200

    1 file modified

--- app/user-information-card/user-information-card.controller.js
+++ app/user-information-card/user-information-card.controller.js
@@ -2,8 +2,8 @@ class UserInformationCardCtrl {
   constructor($scope) {
     "ngInject";
     var ctrl = this;
-    // TODO add your code here.
 
+    // TODO add your code here.
     $scope.name = "Will Hunter";
     $scope.description = "Poolside.Fm";
     $scope.profileimgsrc = "https://rubik.ro/assets/will-hunter.png";
