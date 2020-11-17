bada8a78f52d3884de8ba6852809e03b12d2d273
rubikmm <rubikmm@gmail.com>
2020-11-18 00:08:22 +0200

    3 files modified

--- app/user-information-card/user-information-card.controller.js
+++ app/user-information-card/user-information-card.controller.js
@@ -5,6 +5,8 @@ class UserInformationCardCtrl {
     // TODO add your code here.
 
     $scope.name = "Will Hunter";
+    $scope.description = "Poolside.Fm";
+    $scope.profileimgsrc = "https://rubik.ro/assets/will-hunter.png";
   }
 }
 
