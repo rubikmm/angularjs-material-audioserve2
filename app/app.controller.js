e9ccb575fb76efa5ad1ca475d12f369c6bb250dc
rubikmm <rubikmm@gmail.com>
2020-11-18 00:56:44 +0200

    3 files modified

--- app/app.controller.js
+++ app/app.controller.js
@@ -1,8 +1,17 @@
 class AppCtrl {
-  constructor() {
-    'ngInject';
+  constructor($scope) {
+    "ngInject";
     var ctrl = this;
     // TODO add your code here.
+
+    $scope.primaryMenu = [
+      { label: "Main Tab", value: "pageTab1" },
+      { label: "Main Tab", value: "pageTab2" },
+      { label: "Main Tab", value: "pageTab3" },
+      { label: "Main Tab", value: "pageTab4" },
+      { label: "Main Tab", value: "pageTab5" },
+      { label: "Main Tab", value: "pageTab6" }
+    ];
   }
 }
 
